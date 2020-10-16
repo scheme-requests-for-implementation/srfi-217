86d08f155fb2326a621a2242190070f1ec699050
Wolfgang Corcoran-Mathe <wcm@sigwinch.xyz>
2020-10-16 13:31:42 -0400

    Add iset-delete-min!/-max!.

--- iset-trie.scm
+++ iset-trie.scm
@@ -304,6 +304,53 @@
 (define (iset-delete-all! set ns)
   (iset-delete-all set ns))
 
+;; The search/rebuild process is complicated by the need to handle
+;; a negative branching-bit (which can only occur at the root of the
+;; trie) differently.
+(define (iset-delete-min set)
+  (assume (iset? set))
+  (letrec
+   ((update
+     (lambda (t)
+       (cond ((not t) (values #f #f))
+             ((integer? t) (values t #f))
+             (else (update-branch t)))))
+    (update-branch
+     (lambda (t)
+       (let*-branch (((p m l r) t))
+         (if (negative? m)
+             (let-values (((n r*) (update r)))   ; root node only
+               (values n (branch p m l r*)))
+             (let-values (((n l*) (update l)))
+               (values n (branch p m l* r))))))))
+    (let*-values (((trie) (iset-trie set))
+                  ((least trie*) (update trie)))
+      (values least (raw-iset trie*)))))
+
+(define (iset-delete-max set)
+  (assume (iset? set))
+  (letrec
+   ((update
+     (lambda (t)
+       (cond ((not t) (values #f #f))
+             ((integer? t) (values t #f))
+             (else (update-branch t)))))
+    (update-branch
+     (lambda (t)
+       (let*-branch (((p m l r) t))
+         (if (negative? m)
+             (let-values (((n l*) (update l)))   ; root node only
+               (values n (branch p m l* r)))
+             (let-values (((n r*) (update r)))
+               (values n (branch p m l r*))))))))
+    (let*-values (((trie) (iset-trie set))
+                  ((least trie*) (update trie)))
+      (values least (raw-iset trie*)))))
+
+
+(define (iset-delete-min! set) (iset-delete-min set))
+(define (iset-delete-max! set) (iset-delete-max set))
+
 ;;;; The whole iset
 
 (define (iset-size set)
