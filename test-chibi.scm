a8cb2b56d199e9743ca3837e5e727c39534a0e57
Wolfgang Corcoran-Mathe <wcm@sigwinch.xyz>
2020-10-16 14:06:52 -0400

    More tests.

--- test-chibi.scm
+++ test-chibi.scm
@@ -1,4 +1,5 @@
-(import (iset-trie)
+(import (scheme base)
+        (iset-trie)
         (chibi test)
         (only (srfi 1) iota every)
         )
@@ -35,4 +36,33 @@
   (test-assert (= (length (iset->list pos-set)) (iset-size pos-set)))
   (test-assert (every (lambda (n) (iset-contains? pos-set n))
                       (iset->list pos-set)))
+  )
+
+(test-group "updaters"
+  (test '(1) (iset->list (iset-adjoin (iset) 1)))
+  (test-assert (iset-contains? (iset-adjoin neg-set 10) 10))
+  (test-assert (iset-contains? (iset-adjoin dense-set 100) 100))
+  (test-assert (iset-contains? (iset-adjoin sparse-set 100) 100))
+  (test-equal iset=?
+              (list->iset (cons -3 (iota 20 100 3)))
+              (iset-adjoin pos-set -3))
+
+  (test '() (iset->list (iset-delete (iset 1) 1)))
+  (test-not (iset-contains? (iset-delete neg-set 10) 10))
+  (test-not (iset-contains? (iset-delete dense-set 1033) 1033))
+  (test-not (iset-contains? (iset-delete sparse-set 30) 30))
+  (test-equal iset=?
+              (list->iset (cdr (iota 20 100 3)))
+              (iset-delete pos-set 100))
+  )
+
+(test-group "set theory"
+  (test-equal iset=? mixed-set (iset-union! (iset) mixed-set))
+  (test-equal iset=?
+              (list->iset (append (iota 20 100 3) (iota 20 -100 3)))
+              (iset-union pos-set neg-set))
+  (test-equal iset=? pos-set (iset-union pos-set pos-set))
+  (test-equal iset=?
+              (list->iset (iota 30 100 3))
+              (iset-union pos-set (list->iset (iota 20 130 3))))
   )
