560b3f26f7eeafd58f7d0f0873aa532ce1e93a9a
Wolfgang Corcoran-Mathe <wcm@sigwinch.xyz>
2021-01-04 01:34:39 -0500

    Additional header comments for trie implementation.

--- srfi/trie.scm
+++ srfi/trie.scm
@@ -1,3 +1,19 @@
+;;; This file implements integers sets as compressed binary radix
+;;; trees (AKA Patricia tries), as described by Chris Okasaki and
+;;; Andrew Gill in "Fast Mergeable Integer Maps" (1998).  Integers
+;;; in big-endian binary encoding are stored in a trie structure
+;;; which allows fast lookup, insertion, and set-theoretical
+;;; operations (union, intersection, etc.)
+;;;
+;;; To make efficient use of space, "buddy compression" is used
+;;; to store runs of adjacent integers as bitmaps in single leaves.
+;;; Integers are broken up into a prefix and a string of low-order
+;;; bits; the latter are represented by setting the appropriate bit
+;;; in a leaf's bitmap.  Since a bitmap is represented by a single
+;;; fixnum, we can represent a 5-bit suffix on most 64-bit Schemes,
+;;; and a 4-bit suffix on most 32-bit implementations.  (We pay a
+;;; tax for dynamic typing.)
+;;;
 ;;; A trie is represented by #f (the empty trie), a leaf, or a branch.
 ;;;
 ;;; Throughout this code, the empty trie (#f) is always returned
