f254e205889666e197d3e72c867908e71a3467d5
Wolfgang Corcoran-Mathe <wcm@sigwinch.xyz>
2021-01-29 12:40:03 -0500

    Copy delete-min/-max tests into portable test set.

--- srfi-217-test.scm
+++ srfi-217-test.scm
@@ -83,6 +83,14 @@
     ((_ equal expected expr)
      (check (equal expr expected) => #t))))
 
+;; Reify multiple values as lists.  Unfortunately, this looks rather
+;; ugly on output.  Why doesn't SRFI 78 provide a real MV test form?
+(define-syntax test-values
+  (syntax-rules ()
+    ((test-values expected expr)
+     (check (let-values ((vs expr)) vs)
+      => (let-values ((us expected)) us)))))
+
 ;;;; Utility
 
 (define (print-header message)
@@ -269,6 +277,28 @@
                    (lambda (_ ignore) (ignore #t))
                    (lambda (x _ remove) (remove #t))))
     (lambda (set _) (iset=? (iset 2 4) set))))
+
+  ;;; iset-delete-min / -max
+
+  (test-values (values #f (iset)) (iset-delete-min (iset)))
+  (test-values (values #t #t)
+               (let-values (((n mixed-set*) (iset-delete-min mixed-set)))
+                 (values (= n (car mixed-seq))
+                         (iset=? mixed-set* (list->iset (cdr mixed-seq))))))
+  (test-values (values #t #t)
+               (let-values (((n sparse-set*) (iset-delete-min sparse-set)))
+                 (values (= n (car sparse-seq))
+                         (iset=? sparse-set* (list->iset (cdr sparse-seq))))))
+
+  (test-values (values #f (iset)) (iset-delete-max (iset)))
+  (test-values (values #t #t)
+               (let-values (((n mixed-set*) (iset-delete-max mixed-set)))
+                 (values (= n (last mixed-seq))
+                         (iset=? mixed-set* (list->iset (init mixed-seq))))))
+  (test-values (values #t #t)
+               (let-values (((n sparse-set*) (iset-delete-max sparse-set)))
+                 (values (= n (last sparse-seq))
+                         (iset=? sparse-set* (list->iset (init sparse-seq))))))
   )
 
 (define (check-whole-set)
